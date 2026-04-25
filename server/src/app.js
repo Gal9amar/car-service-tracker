07c1cf5e43f9e06c20d4d4060ed395daed9c2e04
Gal Amar <106630289+Gal9amar@users.noreply.github.com>
2026-04-26 00:04:08 +0300

    fix: trust proxy for express-rate-limit on netlify

--- server/src/app.js
+++ server/src/app.js
@@ -21,6 +21,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
 // ============================================
 // MIDDLEWARE
 // ============================================
